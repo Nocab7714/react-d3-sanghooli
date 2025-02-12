3ae040285edcf905885e2423d5f3a9a1949c3d58
“Yijinggit config --global user.email yijing2055@gmail.comgit initnode_modules/dist/.envgit add .git statusgit commit -m Initial <yijing2055@gmail.com>
2025-02-12 11:23:22 +0800

    feat : 引入 Breadcrumb元件樣式＋局部內容樣式調整

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,20 @@ const { VITE_BASE_URL: baseUrl, VITE_API_PATH: apiPath } = import.meta.env; // �
 // 3.內部資源（排序：元件 -> 工具/樣式等）
 import HeaderFront from './front-end/layout/HeaderFront.jsx';
 import FooterFront from './front-end/layout/FooterFront.jsx';
-import CartPage from './front-end/pages/CartPage.jsx';
 import GoToTopComponent from './front-end/components/GoToTopComponent.jsx';
+
+//import CartPage from './front-end/pages/CartPage.jsx';
+import HowToBuyPage from './front-end/pages/HowToBuyPage.jsx';
+import PrivacyPolicyPage from './front-end/pages/PrivacyPolicyPage.jsx';
 
 
 function App() {
   return (
     <>
       <HeaderFront />
-      <CartPage />
+      {/* <CartPage /> */}
+      {/* <HowToBuyPage /> */}
+      <PrivacyPolicyPage />
       <FooterFront />
       <GoToTopComponent />
     </>
