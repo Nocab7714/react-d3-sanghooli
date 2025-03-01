6c1dd9c6accc799b24bc84f8370aa70dd5936d76
YiJing <yijing2055@gmail.com>
2025-03-01 14:09:26 +0800

    Fix: 修正後臺管理員登入功能

--- src/back-end/Pages/AdminLoginPage.jsx
+++ src/back-end/Pages/AdminLoginPage.jsx
@@ -1,87 +1,91 @@
-import { Outlet, NavLink } from 'react-router-dom';
 import axios from 'axios';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 const routes = [{ path: '/', name: '回到首頁' }];
 
-// const BASE_URL = import.meta.env.VITE_BASE_URL ;
 const { VITE_BASE_URL: baseUrl } = import.meta.env;
 
 
 function AdminLoginPage() {
-    
+   
+  //登入資訊
     const [account, setAccount] = useState({
         username: "example@test.com",
         password: "example",
       });
 
-    const handleInputChange = (e) => {
-    const { value , name } = e.target;
+    const handleInputChange = (event) => {
+    const { value , name } = event.target; // 將點擊後觸發的 event 事件的 value 和 name 解構出來
     
     setAccount({
         ...account,
         [name]:value,
-    });
+      });
     };
     
-    //YJ第二週主線任務資料，改為async await的寫法
-    const handleLogin = async (e)=>{
-    e.preventDefault(); //移除預設觸發行為：防止表單預設提交
+    const navigate = useNavigate();
+    const handleLogin = async (event)=>{
+      event.preventDefault(); //移除預設觸發行為：防止表單預設提交
     
-    try{
-      const res = await axios.post(`${baseUrl}/admin/signin`,account);
-      
-      //透過解構方式，取得：token , expired 資料
-      const { token, expired } = res.data;
-      //將 token 存進 cookie，確保不同頁面可存取
-      document.cookie = `D3Token=${token}; expires=${new Date(expired)}`; 
+      try{
+        const res = await axios.post(`${baseUrl}/admin/signin`,account);
+        
+        //透過解構方式，取得：token , expired 資料
+        const { token, expired } = res.data;
 
-      // 發送請求前，需要在headers 裡帶入 token 資料，後續動作的請求都會自動帶上Token資料
-      axios.defaults.headers.common['Authorization'] = token;
-      
-      getProducts();
+        //將 token 存進 cookie，確保不同頁面可存取
+        document.cookie = `D3Token=${token}; expires=${new Date(expired)}`; 
 
-      // 更新登入狀態：將 setIsAuth 改成 true
-      setIsAuth(true);
-    } catch(error){
-      // console.error("登入失敗", error);
-      alert("登入失敗");
-      }
-    };
+        // 發送請求前，需要在headers 裡帶入 token 資料，後續動作的請求都會自動帶上Token資料
+        axios.defaults.headers.common['Authorization'] = token;
+        
+        alert('登入成功');
+        navigate('/admin/order'); //後台登入直接進到訂單管理頁面
+
+      } catch(error){
+        alert("登入失敗，請稍作等待後，再嘗試重新登入");
+        }
+      };
 
 
     return (
      <>
+       {/* //已完成的登入模板 */}
         <div className="d-flex flex-column justify-content-center align-items-center vh-100">
             <h1 className="mb-5 ">SANGHOOI 後台登入</h1>
-            <form onSubmit={handleLogin} className="d-flex flex-column gap-3">
-                <div className="form-floating mb-3 mt-5">
-                    <input 
-                    name="username" 
-                    value={account.username} 
-                    onChange={handleInputChange} 
-                    type="email" 
-                    className="form-control" 
-                    id="username" 
-                    placeholder="name@example.com" 
-                    />
-                    <label htmlFor="username">Email address</label>
-                </div>
-                <div className="form-floating">
-                    <input 
-                    name="password" 
-                    value={account.password} 
-                    onChange={handleInputChange}  
-                    type="password" 
-                    className="form-control" 
-                    id="password" 
-                    placeholder="Password" 
-                    />
-                    <label htmlFor="password">Password</label>
-                </div>
-                <button type="submit" className="btn btn-primary mt-3">登入</button>
-            </form>
-        </div>
+            <form 
+              onSubmit={handleLogin} 
+              className="d-flex flex-column gap-3"
+            >
+              <div className="form-floating mb-3 mt-5">
+                  <input 
+                  type="email"
+                  className="form-control" 
+                  id="username" 
+                  name="username" 
+                  placeholder="name@example.com"
+                  value={account.username} 
+                  onChange={handleInputChange} 
+                  />
+                  <label htmlFor="username">Email address</label>
+              </div>
+              <div className="form-floating">
+                <input 
+                  type="password" 
+                  className="form-control"
+                  id="password" 
+                  name="password" 
+                  placeholder="Password"
+                  value={account.password} 
+                  onChange={handleInputChange} 
+                  />
+                  <label htmlFor="password">Password</label>
+              </div>
+              <button type="submit" className="btn btn-primary mt-3">登入</button>
+          </form>
+      </div>
 
         {/* 顯示動態內容 */}
         <Outlet/>
